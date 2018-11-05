a8d507c64e7c295506cc32478f7d568d2ef67660
yyIntsuki <intsuki.yz@gmail.com>
2018-11-05 15:05:24 +0100

    Added button eventhandlers.

--- TeamU18NOV05/Form1.cs
+++ TeamU18NOV05/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TeamU18NOV05.DomainModel.Abstract;
+using TeamU18NOV05.DomainModel.Concrete;
 
 namespace TeamU18NOV05
 {
@@ -25,12 +27,48 @@ namespace TeamU18NOV05
             label6.Text = "BLURAYSTATUS";
 
             button1.Text = "PLAY";
+            button1.Click += new EventHandler(videoPlay);
             button3.Text = "PLAY";
+            button3.Click += new EventHandler(dvdPlay);
             button5.Text = "PLAY";
+            button5.Click += new EventHandler(blurayPlay);
 
             button2.Text = "STOP";
+            button2.Click += new EventHandler(videoStop);
             button4.Text = "STOP";
+            button4.Click += new EventHandler(dvdStop);
             button6.Text = "STOP";
+            button6.Click += new EventHandler(blurayStop);
+        }
+
+        private void videoPlay(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dvdPlay(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void blurayPlay(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void videoStop(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dvdStop(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void blurayStop(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
